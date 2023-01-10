5a484264a34f5425d0cf56d0b610724f591ea851
BrandonAWong <brandon.alex.wong@gmail.com>
2023-01-09 23:59:09 -0800

    extrated code and added retrieval of definitions for incorrect words

--- main.py
+++ main.py
@@ -2,31 +2,48 @@ import requests
 import random
 from random_word import RandomWords
 
-while True:
-    correctWord = RandomWords().get_random_word()
-    wordChoices = [correctWord]
+def getWordsWithDefs():
+    wordChoices = []
+    definitions = []
+    for _ in range(4):
+        randomWord = RandomWords().get_random_word()
+        getDefinition(randomWord, definitions)
+        wordChoices.append(randomWord)
 
-    try:
-        url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{correctWord}"
-        info = requests.get(url).json()[0]
-        definition = info['meanings'][0]['definitions'][0]
-        print(f'If the definition of the word is:\n{definition["definition"]}\n')
-        for i in range(3):
-            wordChoices.append(RandomWords().get_random_word())
+    return wordChoices, definitions
 
-        random.shuffle(wordChoices)
+def getDefinition(word, definitions):
+    while True:
+        try:
+            url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+            info = requests.get(url).json()[0]
+            return definitions.append(info['meanings'][0]['definitions'][0]['definition'])
 
-        for index, word in enumerate(wordChoices):
-            print(f'{index+1}) {word}')
+        except:
+            print(word, "shit aint here")
+            word = RandomWords().get_random_word()
 
-        answer = input('\nThen the word is: ')
-        if answer == correctWord:
-            print("🎉🥳🎉")
 
-        else:
-            print(f'😭 the answer was {correctWord}')
 
-        break
 
-    except:
-        pass
+if __name__ == '__main__':
+    words, definitions = getWordsWithDefs()
+    correctWord = random.choice(words)
+    correctIndex = words.index(correctWord)
+    print(f'If the definition of the word is:\n{definitions[correctIndex]}\n')
+
+    for index, word in enumerate(words):
+        print(f'{index+1}) {word}')
+    
+    answer = input('\nThen the word is: ')
+    if answer == correctWord:
+        print("🎉🥳🎉")
+
+    else:
+        print(f'😭 the answer was {correctWord}')
+    
+    words.pop(correctIndex)
+    definitions.pop(correctIndex)
+    print('\nThe rest of the words:')
+    for index, word in enumerate(words):
+        print(f'{word} - {definitions[index]}')
