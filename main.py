d0565ac41f320f9515647dd8fbaee056d4542b0d
BrandonAWong <brandon.alex.wong@gmail.com>
2023-01-10 18:21:08 -0800

    made word class

--- main.py
+++ main.py
@@ -2,38 +2,47 @@ import requests
 import random
 from random_word import RandomWords
 
-def getWordsWithDefs():
-    wordChoices = []
-    definitions = []
-    for _ in range(4):
-        randomWord = RandomWords().get_random_word()
-        getDefinition(randomWord, definitions)
-        wordChoices.append(randomWord)
+class Word:
+    def __init__(self):
+        self.word = None
+        self.definition = None
 
-    return wordChoices, definitions
+    def setWord(self, word):
+        self.validateWord(word)
+        self.word = word
 
-def getDefinition(word, definitions):
-    while True:
+    def validateWord(self, word):
         try:
             url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
             info = requests.get(url).json()[0]
-            return definitions.append(info['meanings'][0]['definitions'][0]['definition'])
+            self.setDefinition(info)
 
         except:
             print(word, "shit aint here")
-            word = RandomWords().get_random_word()
+            self.setWord(RandomWords().get_random_word())
 
 
+    def setDefinition(self, json):
+        self.definition = json['meanings'][0]['definitions'][0]['definition']
 
+
+def getListWordDef():
+    wordChoices = []
+    for _ in range(4):
+        word = Word()
+        word.setWord(RandomWords().get_random_word())
+        wordChoices.append(word)
+
+    return wordChoices
 
 if __name__ == '__main__':
-    words, definitions = getWordsWithDefs()
-    correctWord = random.choice(words)
-    correctIndex = words.index(correctWord)
-    print(f'If the definition of the word is:\n{definitions[correctIndex]}\n')
+    words = getListWordDef()
+    correctOption = random.choice(words)
+    correctWord = correctOption.word
+    print(f'If the definition of the word is:\n{correctOption.definition}\n')
 
     for index, word in enumerate(words):
-        print(f'{index+1}) {word}')
+        print(f'{index+1}) {word.word}')
     
     answer = input('\nThen the word is: ')
     if answer == correctWord:
@@ -41,9 +50,8 @@ if __name__ == '__main__':
 
     else:
         print(f'😭 the answer was {correctWord}')
-    
-    words.pop(correctIndex)
-    definitions.pop(correctIndex)
+
+    words.remove(correctOption)
     print('\nThe rest of the words:')
     for index, word in enumerate(words):
-        print(f'{word} - {definitions[index]}')
+        print(f'{word.word} - {word.definition}')
