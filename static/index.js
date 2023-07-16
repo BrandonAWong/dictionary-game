fe7004647cf8ee1d1980c1bceb06d8dff5a1d2b0
BrandonAWong <brandon.alex.wong@gmail.com>
2023-07-16 15:54:52 -0700

    base gamepage loads properly with required info

--- static/index.js
+++ static/index.js
@@ -1,1 +1,18 @@
-console.log('hi');
+function createChoices(data)
+{
+    const options = JSON.parse(data);
+    for (let i = 0; i < 4; i++) 
+    {
+        const word = options[i].Word;
+        const button = document.createElement("button")
+        button.className = "choice"
+        button.onclick = checkAnswer(word, data);
+        button.innerHTML = word;
+        document.body.appendChild(button);
+    }
+}
+
+function checkAnswer(word, data)
+{
+    console.log(word);
+}
--- views.py
+++ views.py
@@ -1,16 +1,20 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, jsonify
+from csv import DictReader
+from random import choice
 
 
 views = Blueprint(__name__, 'views')
+with open('wordlist.csv', 'r', encoding='cp850') as word_list:
+    bank: DictReader = DictReader(word_list)
+    dictionary: list[dict] = [row for row in bank]
 
 @views.route('/')
 def home():
-    return render_template('index.html', name='Brandon')
+    options = [choice(dictionary) for _ in range(4)]
+    correct_option = choice(options)
+    c_definition = correct_option['Definition'].replace(correct_option['Word'], '______')
+    return render_template('index.html', definition=c_definition, data=options)
 
-@views.route('/profile/<username>')
-def profile(username):
-    return render_template('index.html', name=username)
-
-@views.route('go-home')
-def go_home():
-    return redirect(url_for('views.home'))
+@views.route('/login')
+def login():
+    return render_template('login.html')
