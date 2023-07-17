e2001d6d37b8817296049dedf0b237bba9e15676
BrandonAWong <brandon.alex.wong@gmail.com>
2023-07-16 19:15:51 -0700

    base game fully functional

--- static/index.js
+++ static/index.js
@@ -1,18 +1,51 @@
 function createChoices(data)
 {
+    const buttons = document.getElementsByClassName("choice")
     const options = JSON.parse(data);
     for (let i = 0; i < 4; i++) 
     {
+        const button = buttons[i]
         const word = options[i].Word;
-        const button = document.createElement("button")
-        button.className = "choice"
-        button.onclick = checkAnswer(word, data);
+        const definition = options[i].Definition;
+        button.addEventListener("click", checkAnswer);
         button.innerHTML = word;
-        document.body.appendChild(button);
+        button.definition = definition;
     }
 }
 
-function checkAnswer(word, data)
+function checkAnswer(evt)
 {
-    console.log(word);
+    const button = evt.currentTarget
+    button.disabled = true;
+    let answerDef = document.getElementById("card-info")
+    answerDef = answerDef.innerHTML.replace("______", button.innerHTML)
+    const choiceDef = button.definition
+
+    if (choiceDef === answerDef)
+    {
+        button.className = "choice correct";
+        win()
+    }
+    else
+    {
+        button.className = "choice incorrect";
+    }
+}
+
+function win() 
+{
+    const choices = document.getElementsByClassName("choice");
+    for (const btn of choices) 
+    {
+        btn.disabled = true;
+    }
+    document.getElementById("win").hidden = false;
+
+    const nextButton = document.getElementById("next");
+    nextButton.addEventListener("click", next);
+}
+
+function next()
+{
+    location.reload();
 }
